9ad867af0e5874f5cf2cb009db18e7392d6c9cbb
Shubhambisht487 <60968658+Shubhambisht487@users.noreply.github.com>
2021-05-13 10:41:46 +0530

    first commit

---
+++ FirstAzureWebService/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAzureWebService.Models;
+namespace FirstAzureWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //List<Employee> Employee=new List<Employee>();
+        
+            private EmployeeDbContext _context;
+        public EmployeeController(EmployeeDbContext context)
+        {
+
+            _context = context;
+            if (!_context.Employees.Any())
+            {
+                _context.Employees.Add(new Employee
+                {Id=10,Name="Chris Gayle",Salary=100000,Department="Cricket" });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<Employee> GetWorkshops()
+        {
+            return _context.Employees;
+        }
+
+    }
+}
---
+++ FirstAzureWebService/Models/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAzureWebService.Models
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> context) : base(context)
+        { 
+        
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
